741c30f512654b0e9b670fd6226aa7333cbee99d
xuanngoc <bx.ngoc.99@gmail.com>
2018-09-18 21:44:59 +0700

    methods

--- queue/queue.h
+++ queue/queue.h
@@ -1,7 +1,7 @@
 #ifndef queue_h
 #define queue_h
 #include "node.h"
-
+#include <fstream>
 template <class T>
 class Queue
 {
@@ -10,8 +10,55 @@ private:
 public:
 	Queue(){
 		queue =0;
-	};
-	void EnQueue(T val)
+	}
+	Queue(T const &value, int const &loop){
+		queue = 0;
+		for(int i=0; i< loop; i++){
+
+			Node<T> *n = new Node<T>;
+			n-> data = value;
+			n-> next = 0;
+			if(queue == 0)
+			{	
+				queue = n;
+			}else
+			{
+				Node<T> * p = queue;
+				while(p->next != 0)
+				{
+					p=p->next;
+				}
+				p->next = n;		
+			}	
+		}
+			
+	}
+	Queue(char * filename){
+		queue =0;
+		ifstream fin(filename);
+		//cout<<"filename = "<<filename<<endl;
+		if(!fin.is_open()){
+			cout<<"File isn't exist !!!\n";
+			return;
+		}
+		while(!fin.eof()){
+			T temp;
+			fin>>temp;
+			Node<T> * n = new Node<T>(temp);
+			if(queue ==0){
+				queue = n;
+			}else{
+				Node<T> *p = queue;
+				while(p->next !=0){
+					p=p->next;
+				}
+				p->next = n;
+			}
+		}
+		fin.close();
+	}
+
+	void EnQueue(T const &val)
 	{	
 		Node<T> *n = new Node<T>;
 		n-> data = val;
@@ -39,6 +86,122 @@ public:
 	}
 	bool isEmpty()const{
 		return queue ==0;
+	}
+	void deleteAt(int const &pos){
+		if(pos == 1){
+			Node<T> * q = queue;
+			queue= queue->next;
+			delete q;
+		}else{
+			Node<T> * p = queue;
+			for(int i=1; i< pos-1; i++){
+				p = p->next;
+			}
+			Node<T> *q = p->next;
+			p->next = p->next->next;
+			delete q;
+		}
+	}
+	void sort(){
+		Node<T> * p = queue;
+		while(p != 0){
+			Node<T> * q = p->next;
+			while(q != 0){
+				if(p-> data > q->data){
+					T t = p->data;
+					p->data = q->data;
+					q->data = t;
+				}
+				q = q->next;
+			}
+			p=p->next;
+		}
+	}
+	bool has(T const &value){
+		Node<T> * p = queue;
+		while(p != 0){
+			if(p->data == value){
+				return 1;
+			}
+			p=p->next;
+		}
+		return 0;
+	}
+	T top()const{
+		return queue->data;
+	}
+	T last()const{
+		Node<T> * p = queue;
+		while( p->next != 0){
+			p=p->next;
+		}
+		return p-> next;
+	}
+
+	void operator + (T const &value){
+		Node<T> *p = queue;
+		while(p != 0){
+			p->data += value;
+			p = p->next;
+		}
+	}
+	void operator - (T const &value){
+		Node<T> *p = queue;
+		while(p != 0){
+			p->data -= value;
+			p = p->next;
+		}
+	}
+	void operator * (T const &value){
+		Node<T> *p = queue;
+		while(p != 0){
+			p->data *= value;
+			p = p->next;
+		}
+	}
+	void operator / (T const &value){
+		Node<T> *p = queue;
+		while(p != 0){
+			p->data /= value;
+			p = p->next;
+		}
+	}
+
+	void swap(int const & pos1, int const &pos2){
+		Node<T> *p =queue;
+		for(int i=1; i< pos1; i++){
+			p=p->next;
+		}
+		Node<T> *q =queue;
+		for(int i=1; i< pos2; i++){
+			q=q->next;
+		}
+		T temp = p->data;
+		p->data = q->data;
+		q->data =temp;
+	}
+	void swap(Queue<T> &qu){
+		Queue<T> t = *this;
+		*this = qu;
+		qu = t;
+	}
+	int size()const{
+		Node<T> * p =queue;
+		int counter=0;
+		while(p != 0){
+			counter++;
+			p=p->next;
+		}
+		return counter;
+
+	}
+	void print()const{
+		Node<T> * p =queue;
+		while(p != 0){
+			cout<<p->data<<"  ";
+			p=p->next;
+		}
+		cout<<endl;
 	}
 };
 #endif
--- stack/stack.h
+++ stack/stack.h
@@ -2,22 +2,61 @@
 #define stack_h
 
 #include "node.h"
+#include <fstream>
 
 template <class T>
 class Stack
 {
+private:
+	Node <T> * top;
+	
+
 public:
 	Stack()
 	{
 		top = 0;
 	};
-	void Push(T val)
+	Stack(T const & val, int const &loop ){
+		top = 0;
+		for(int i=0; i< loop; i++){
+			Node<T> * p = new Node<T>(val);
+			p->next = top;
+			top = p;
+		}
+	}
+	Stack(char * filename){
+		ifstream fin(filename);
+		if(!fin.is_open() ){
+			cout<<"Can't open file !!!\n";
+			return;
+		}
+		top = 0;
+		while(!fin.eof()){
+			T t;
+			fin >>t;
+			Node<T> *p = new Node<T>(t);
+			p->next = top;
+			top = p;	
+		}
+		fin.close();
+	}
+
+	void Push(T const &val)
 	{
 		Node<T> * p = new Node<T>;
 		p->data = val;
 		p->next = top;
 		top = p;
 	};
+	void Push(Stack st){
+		Node <T> * q = st.top;
+		while(q != 0){
+			Node<T> * t = new Node<T>(st.Pop());
+			t->next = top;
+			top = t;
+			q=st.top;
+		}
+	}
 	T Pop()
 	{
 		T t = top->data;
@@ -26,11 +65,119 @@ public:
 		delete q;
 		return t;
 	};
+	T getTop()const{
+		return top->data;
+	}
+	void deleteAt(int const & pos){
+		Node<T> * p = top;
+		for(int i=0; i< pos -1; i++){
+			p=p->next;
+		}
+		Node<T> * q = p-> next;
+		p->next= p->next->next;
+		delete q;
+	}
+	void swap(int const &pos1, int const &pos2){
+		Node<T> * p = top;
+		for(int i=1; i< pos1; i++){
+			p=p->next;
+		}
+		Node<T> * q = top;
+		for(int i=1; i< pos2; i++){
+			q=q->next;
+		}
+		T t = p->data;
+		p->data = q->data;
+		q->data = t;
+	}
+	void swap(Stack &st){
+		Node<T> * p  = top;
+		top = st.top;
+		st.top= p;   
+	}
 	bool isEmpty()const{
 		return top ==0;
 	}
-private:
-	Node <T> * top;
-	
+	void sort(){
+		Node<T> *p = top;
+		while(p != 0){
+			Node<T> * q= p->next;
+			while( q != 0){
+				if( p-> data > q->data){
+					T t = p->data;
+					p->data = q->data;
+					q->data = t;
+				}
+				q=q->next;
+			}
+			p=p->next;
+		}
+	}
+	int size()const{
+		int counter=0;
+		Node<T> * p =top;
+		while(p != top){
+			counter++;
+			p=p->next;
+		}
+		return counter;
+	}
+	int count(T const &val){
+		int c=0;
+		Node<T> * p = top;
+		while(p != 0){
+			if(p->data == val){
+				c++;
+			}
+			p=p->next;
+		}
+		return c;
+	}
+	void has(T const &val){
+		Node<T> * p = top;
+		while(p != 0){
+			if(p->data = val){
+				return 1;
+			}
+			p=p->next;
+		}
+		return 0;
+	}
+	void operator + (T const &value){
+		Node <T> * p = top;
+		while(p != 0){
+			p->data += value;
+			p=p->next;
+		}
+	}
+	void operator - (T const &value){
+		Node <T> * p = top;
+		while(p != 0){
+			p->data -= value;
+			p=p->next;
+		}
+	}
+	void operator *(T const &value){
+		Node <T> * p = top;
+		while(p != 0){
+			p->data *= value;
+			p=p->next;
+		}
+	}
+	void operator /(T const &value){
+		Node <T> * p = top;
+		while(p != 0){
+			p->data /= value;
+			p=p->next;
+		}
+	}
+	void print()const{
+		Node<T> *p =top;
+		while(p != 0){
+			cout<<p -> data<<"  ";
+			p=p->next;
+		}
+		cout <<endl;
+	}
 };
 #endif
